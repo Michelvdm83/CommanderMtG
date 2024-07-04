632b790a0c3b80649481995af494ad7deef3a4b5
Michelvdm83 <michelvdm@gmail.com>
2024-07-04 14:33:07 +0200

    new stuff shouldn.t be active

--- backend/src/main/java/com/mvdmstudy/mtg/commander/card/NewCard.java
+++ backend/src/main/java/com/mvdmstudy/mtg/commander/card/NewCard.java
@@ -1,46 +1,46 @@
-package com.mvdmstudy.mtg.commander.card;
-
-import com.mvdmstudy.mtg.commander.cardface.CardFace;
-import com.mvdmstudy.mtg.commander.setData.SetData;
-import jakarta.persistence.Column;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import java.util.Set;
-import java.util.UUID;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class NewCard {
-    private UUID id;
-
-    private UUID oracleId;
-
-    private String name;
-
-    private String manaCost;
-    private Integer cmc;
-
-    private String types;
-
-    @Column(columnDefinition = "TEXT")
-    private String text;
-
-    private Integer power;
-    private Boolean isPowerStarred;
-
-    private Integer toughness;
-    private Boolean isToughnessStarred;
-
-    private String imageURL;
-
-    private boolean commanderLegal;
-
-    //    @ManyToOne
-    private SetData set;
-
-    // @OneToMany (mappedBy = "card", cascade = CascadeType.ALL)
-    private Set<CardFace> cardFaces;
-}
+//package com.mvdmstudy.mtg.commander.card;
+//
+//import com.mvdmstudy.mtg.commander.cardface.CardFace;
+//import com.mvdmstudy.mtg.commander.setData.SetData;
+//import jakarta.persistence.Column;
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//import java.util.Set;
+//import java.util.UUID;
+//
+//@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
+//public class NewCard {
+//    private UUID id;
+//
+//    private UUID oracleId;
+//
+//    private String name;
+//
+//    private String manaCost;
+//    private Integer cmc;
+//
+//    private String types;
+//
+//    @Column(columnDefinition = "TEXT")
+//    private String text;
+//
+//    private Integer power;
+//    private Boolean isPowerStarred;
+//
+//    private Integer toughness;
+//    private Boolean isToughnessStarred;
+//
+//    private String imageURL;
+//
+//    private boolean commanderLegal;
+//
+//    //    @ManyToOne
+//    private SetData set;
+//
+//    // @OneToMany (mappedBy = "card", cascade = CascadeType.ALL)
+//    private Set<CardFace> cardFaces;
+//}
--- backend/src/main/java/com/mvdmstudy/mtg/commander/cardface/CardFace.java
+++ backend/src/main/java/com/mvdmstudy/mtg/commander/cardface/CardFace.java
@@ -1,40 +1,40 @@
-package com.mvdmstudy.mtg.commander.cardface;
-
-import com.mvdmstudy.mtg.commander.card.Card;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import java.util.UUID;
-
-@Entity
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
-public class CardFace {
-    @Id
-    @GeneratedValue
-    private UUID id;
-
-    private String name;
-    private String manaCost;
-    private String types;
-
-    @Column(columnDefinition = "TEXT")
-    private String text;
-
-    private Integer power;
-    private Boolean isPowerStarred;
-
-    private Integer toughness;
-    private String isToughnessStarred;
-
-    private String imageURL;
-
-    // @ManyToOny
-    private Card card;
-}
+//package com.mvdmstudy.mtg.commander.cardface;
+//
+//import com.mvdmstudy.mtg.commander.card.Card;
+//import jakarta.persistence.Column;
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.Id;
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//import java.util.UUID;
+//
+//@Entity
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Data
+//public class CardFace {
+//    @Id
+//    @GeneratedValue
+//    private UUID id;
+//
+//    private String name;
+//    private String manaCost;
+//    private String types;
+//
+//    @Column(columnDefinition = "TEXT")
+//    private String text;
+//
+//    private Integer power;
+//    private Boolean isPowerStarred;
+//
+//    private Integer toughness;
+//    private String isToughnessStarred;
+//
+//    private String imageURL;
+//
+//    // @ManyToOny
+//    private Card card;
+//}
